59656a3b9672ee390ba9f4622312c1420479d44e
Bernd Klaiber <bk@bkedv.de>
2017-01-17 19:48:13 +0100

    duplicate 'Key' in Name fixed

--- WpfDemoApp/Images/Icons.cs
+++ WpfDemoApp/Images/Icons.cs
@@ -6,30 +6,30 @@ namespace WpfDemoApp.Images
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public static class Icons
     {
-        private static readonly ComponentResourceKey JOGKeyGeometry1KeyCompResKey = MakeKey("Icons.JOGKeyGeometry1Key");
+        private static readonly ComponentResourceKey JOGGeometry1KeyCompResKey = MakeKey("Icons.JOGGeometry1Key");
 
-        public static ResourceKey JOGKeyGeometry1Key => JOGKeyGeometry1KeyCompResKey;
-        private static readonly ComponentResourceKey JOGKeyGeometry2KeyCompResKey = MakeKey("Icons.JOGKeyGeometry2Key");
+        public static ResourceKey JOGGeometry1Key => JOGGeometry1KeyCompResKey;
+        private static readonly ComponentResourceKey JOGGeometry2KeyCompResKey = MakeKey("Icons.JOGGeometry2Key");
 
-        public static ResourceKey JOGKeyGeometry2Key => JOGKeyGeometry2KeyCompResKey;
+        public static ResourceKey JOGGeometry2Key => JOGGeometry2KeyCompResKey;
         private static readonly ComponentResourceKey JOGDrawingGroupKeyCompResKey = MakeKey("Icons.JOGDrawingGroupKey");
 
         public static ResourceKey JOGDrawingGroupKey => JOGDrawingGroupKeyCompResKey;
         private static readonly ComponentResourceKey JOGDrawingImageKeyCompResKey = MakeKey("Icons.JOGDrawingImageKey");
 
         public static ResourceKey JOGDrawingImageKey => JOGDrawingImageKeyCompResKey;
-        private static readonly ComponentResourceKey cloud_3_iconKeyGeometryKeyCompResKey = MakeKey("Icons.cloud_3_iconKeyGeometryKey");
+        private static readonly ComponentResourceKey cloud_3_iconGeometryKeyCompResKey = MakeKey("Icons.cloud_3_iconGeometryKey");
 
-        public static ResourceKey cloud_3_iconKeyGeometryKey => cloud_3_iconKeyGeometryKeyCompResKey;
+        public static ResourceKey cloud_3_iconGeometryKey => cloud_3_iconGeometryKeyCompResKey;
         private static readonly ComponentResourceKey cloud_3_iconDrawingGroupKeyCompResKey = MakeKey("Icons.cloud_3_iconDrawingGroupKey");
 
         public static ResourceKey cloud_3_iconDrawingGroupKey => cloud_3_iconDrawingGroupKeyCompResKey;
         private static readonly ComponentResourceKey cloud_3_iconDrawingImageKeyCompResKey = MakeKey("Icons.cloud_3_iconDrawingImageKey");
 
         public static ResourceKey cloud_3_iconDrawingImageKey => cloud_3_iconDrawingImageKeyCompResKey;
-        private static readonly ComponentResourceKey _3d_view_iconKeyGeometryKeyCompResKey = MakeKey("Icons._3d_view_iconKeyGeometryKey");
+        private static readonly ComponentResourceKey _3d_view_iconGeometryKeyCompResKey = MakeKey("Icons._3d_view_iconGeometryKey");
 
-        public static ResourceKey _3d_view_iconKeyGeometryKey => _3d_view_iconKeyGeometryKeyCompResKey;
+        public static ResourceKey _3d_view_iconGeometryKey => _3d_view_iconGeometryKeyCompResKey;
         private static readonly ComponentResourceKey _3d_view_iconDrawingGroupKeyCompResKey = MakeKey("Icons._3d_view_iconDrawingGroupKey");
 
         public static ResourceKey _3d_view_iconDrawingGroupKey => _3d_view_iconDrawingGroupKeyCompResKey;
