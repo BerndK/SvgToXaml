99cfd01a1382be5a494a5e0796c1c0e2d6689ec0
BerndK <bk@bkedv.de>
2019-02-13 19:13:47 +0100

    UnitTests for many files

--- SvgConverterTest/ConverterTests.cs
+++ SvgConverterTest/ConverterTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Media;
 using System.Xml.Linq;
@@ -16,6 +17,27 @@ namespace SvgConverterTest
     [TestFixture]
     public class ConverterTests
     {
+        private void CheckXamlOutput(string xaml, string testSuffix, bool check = true, [CallerMemberName] string testName = null)
+        {
+            this.CheckXamlOutput(xaml, check, testName, testSuffix);
+        }
+
+        private void CheckXamlOutput(string xaml, bool check = true, [CallerMemberName]  string testName = null, string testSuffix = null)
+        {
+            Console.WriteLine(xaml);
+            Directory.CreateDirectory("TestFiles\\Actual");
+            var filename = testName;
+            if (testSuffix != null)
+                filename += $"_{testSuffix}";
+            filename += ".xaml";
+            File.WriteAllText($"TestFiles\\Actual\\{filename}", xaml);
+            if (check)
+            {
+                string expected = File.ReadAllText($"TestFiles\\Expected\\{filename}");
+                xaml.Should().Be(expected);
+            }
+        }
+
         [TestCase("TestFiles\\cloud-3-icon.svg")]
         [TestCase("TestFiles\\3d-view-icon.svg")]
         [TestCase("TestFiles\\JOG.svg")]
@@ -29,7 +51,8 @@ namespace SvgConverterTest
             };
             var resKeyInfo = new ResKeyInfo {Prefix = "Prefix"};
             var xaml = ConverterLogic.SvgFileToXaml(filename, ResultMode.DrawingGroup, resKeyInfo, settings);
-            Console.WriteLine(xaml);
+
+            CheckXamlOutput(xaml, Path.GetFileName(filename));
         }
 
         [Test]
@@ -43,9 +66,7 @@ namespace SvgConverterTest
             };
             var resKeyInfo = new ResKeyInfo { Prefix = "Prefix" };
             var xaml = ConverterLogic.SvgFileToXaml("TestFiles\\3d-view-icon.svg", ResultMode.DrawingGroup, resKeyInfo, settings);
-            Console.WriteLine(xaml);
-            string expected = File.ReadAllText("TestFiles\\3d-view-icon_expected.txt");
-            xaml.Should().Be(expected);
+            CheckXamlOutput(xaml);
         }
 
         [TestCase("TestFiles\\cloud-3-icon.svg")]
@@ -61,7 +82,7 @@ namespace SvgConverterTest
             };
             var resKeyInfo = new ResKeyInfo { Prefix = "Prefix" };
             var xaml = ConverterLogic.SvgFileToXaml(filename, ResultMode.DrawingImage, resKeyInfo, settings);
-            Console.WriteLine(xaml);
+            CheckXamlOutput(xaml, Path.GetFileName(filename));
         }
 
         [TestCase("TestFiles\\cloud-3-icon.svg")]
@@ -76,7 +97,7 @@ namespace SvgConverterTest
             };
             var resKeyInfo = new ResKeyInfo { Prefix = "Prefix" };
             var xaml = ConverterLogic.SvgFileToXaml(filename, ResultMode.DrawingGroup, resKeyInfo, settings);
-            Console.WriteLine(xaml);
+            CheckXamlOutput(xaml, Path.GetFileName(filename));
         }
 
         [Test]
@@ -90,11 +111,7 @@ namespace SvgConverterTest
             };
             var resKeyInfo = new ResKeyInfo { XamlName = "Test", Prefix = "NamePrefix" };
             var xaml = ConverterLogic.SvgDirToXaml("TestFiles\\", resKeyInfo, settings);
-            Console.WriteLine(xaml);
-            //File.WriteAllText("TestFiles\\ExpectedXaml_SvgDirToXamlTest_withNamePrefix_.xaml", xaml);
-            var expectedXaml = File.ReadAllText("TestFiles\\ExpectedXaml_SvgDirToXamlTest_withNamePrefix.xaml");
-            Assert.That(xaml, Is.EqualTo(expectedXaml));
-            //xaml.Should().Be(expectedXaml);
+            CheckXamlOutput(xaml);
         }
 
         [Test]
@@ -108,10 +125,7 @@ namespace SvgConverterTest
             };
             
             var xaml = ConverterLogic.SvgDirToXaml("TestFiles\\", ResKeyInfoUseCompResKey, settings);
-            Console.WriteLine(xaml);
-            File.WriteAllText("TestFiles\\ExpectedXaml_SvgDirToXamlTest_withUseCompResKey.xaml", xaml);
-            var expectedXaml = File.ReadAllText("TestFiles\\ExpectedXaml_SvgDirToXamlTest_withUseCompResKey.xaml");
-            xaml.Should().Be(expectedXaml);
+            CheckXamlOutput(xaml);
         }
 
         [Test]
@@ -119,7 +133,7 @@ namespace SvgConverterTest
         {
             var resKeyInfo = new ResKeyInfo { XamlName = "Test", Prefix = "NamePrefix" };
             var xaml = ConverterLogic.SvgDirToXaml("TestFiles\\", resKeyInfo, null);
-            Console.WriteLine(xaml);
+            CheckXamlOutput(xaml);
         }
 
         [Test, STAThread]
@@ -127,8 +141,7 @@ namespace SvgConverterTest
         {
             var resKeyInfo = new ResKeyInfo { Prefix = "Prefix" };
             var xaml = ConverterLogic.SvgFileToXaml("TestFiles\\Handwheel.svg", ResultMode.DrawingGroup, resKeyInfo, null);
-            Console.WriteLine(xaml);
-            Clipboard.SetText(xaml);
+            CheckXamlOutput(xaml);
         }
         [Test, STAThread]
         public void Handwheel1() //pure svg# without any modifications
@@ -137,8 +150,7 @@ namespace SvgConverterTest
             DrawingGroup drawing = fileReader.Read("TestFiles\\Handwheel.svg");
             XmlXamlWriter writer = new XmlXamlWriter(null);
             var xaml = writer.Save(drawing);
-            Console.WriteLine(xaml);
-            Clipboard.SetText(xaml);
+            CheckXamlOutput(xaml);
         }
         [Test, STAThread]
         public void Handwheel2() //integrated conversion, manual writing
@@ -146,16 +158,14 @@ namespace SvgConverterTest
             var drawing = SvgConverter.ConverterLogic.SvgFileToWpfObject("TestFiles\\Handwheel.svg", null);
             XmlXamlWriter writer = new XmlXamlWriter(null);
             var xaml = writer.Save(drawing);
-            Console.WriteLine(xaml);
-            Clipboard.SetText(xaml);
+            CheckXamlOutput(xaml);
         }
         [Test, STAThread]
         public void Handwheel3() //integrated conversion, integrated writing
         {
             var drawing = ConverterLogic.SvgFileToWpfObject("TestFiles\\Handwheel.svg", null);
             var xaml = ConverterLogic.SvgObjectToXaml(drawing, true, "Test");
-            Console.WriteLine(xaml);
-            Clipboard.SetText(xaml);
+            CheckXamlOutput(xaml);
         }
 
         private static ResKeyInfo ResKeyInfoUseNamePrefix
